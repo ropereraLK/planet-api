9d1209e62c745d15fcf75a16215960246fa8269b
Rohan Perera <ropereralk@gmail.com>
2020-09-02 21:03:17 +0530

    Simple JUnit Test passed for Controller 2

---
+++ src/test/java/com/planet/controller/PlanetControllerInitTest.java
@@ -0,0 +1,33 @@
+package com.planet.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(PlanetController.class)
+class PlanetControllerInitTest {
+	
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void test() throws Exception {
+		
+		
+		RequestBuilder request = MockMvcRequestBuilders.get("/");
+		MvcResult result = mvc.perform(request).andReturn();
+		assertEquals("Hello jupiter", result.getResponse().getContentAsString() );
+	}
+
+}
