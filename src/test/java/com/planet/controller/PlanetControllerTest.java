1cff9f0716f7058ae68f3f3482c5d51629f636fa
Rohan Perera <ropereralk@gmail.com>
2020-09-02 20:07:49 +0530

    Simple JUnit Test passed for Controller

---
+++ src/test/java/com/planet/controller/PlanetControllerTest.java
@@ -0,0 +1,25 @@
+package com.planet.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.sym.Name;
+
+class PlanetControllerTest {
+	
+	
+
+	@RepeatedTest(300)
+	void test() {
+
+		PlanetController pc = new PlanetController();
+		String response = pc.planetInfo("Mars");
+	
+		 
+		assertEquals("Hello Mars", response);
+
+	}
+
+}
