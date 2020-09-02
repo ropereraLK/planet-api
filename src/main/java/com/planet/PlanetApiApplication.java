ef2dc5528bdc63361cba9dbc6b912f2354e6f497
Rohan Perera <ropereralk@gmail.com>
2020-09-02 18:25:28 +0530

    initial commit

---
+++ src/main/java/com/planet/PlanetApiApplication.java
@@ -0,0 +1,13 @@
+package com.planet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planet/PlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
